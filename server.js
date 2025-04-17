125c6241a86e18df1d9b7ed09f298bc829faaeeb
Zuri kim <devzuri97@gmail.com>
2025-04-17 17:25:02 +0900

    Update email sending functionality

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/process', (req, res) => {
